82b0bba420ee62bef85f8d161d8bbb67abe22eaa
Joe Buckley <26873231+akabucko@users.noreply.github.com>
2026-08-27 15:01:19 +0100

    Updated: components added

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,20 @@
 import Alpine from 'alpinejs';
+import Swiper from 'swiper/bundle';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 // Components
 
+// Helpers
+import gsapHorizontalLoop from './helpers/gsapHorizontalLoop.js';
+
 // Global
 window.Alpine = Alpine;
+window.Swiper = Swiper;
+window.gsap = gsap;
+window.ScrollTrigger = ScrollTrigger;
+window.gsapHorizontalLoop = gsapHorizontalLoop;
 
 // Inits
+gsap.registerPlugin(ScrollTrigger);
 Alpine.start();
